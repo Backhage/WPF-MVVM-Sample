2b9dcacfab351749735f13df5c163eb356b5ca80
Eric Backhage <eric.backhage@gmail.com>
2017-12-18 21:40:36 +0100

    Made it possible to select and delete users from the viewmodel using a RelayCommand called from the view.

---
+++ AppWithGui/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AppWithGui
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> command;
+        private readonly Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> command) : this(command, null)
+        {
+        }
+
+        public RelayCommand(Action<object> command, Predicate<object> canExecute)
+        {
+            this.command = command ?? throw new ArgumentNullException(nameof(command));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            command.Invoke(parameter);
+        }
+    }
+}
--- AppWithGui/ViewModel/UserViewModel.cs
+++ AppWithGui/ViewModel/UserViewModel.cs
@@ -6,10 +6,13 @@ namespace AppWithGui.ViewModel
     internal class UserViewModel
     {
         public ObservableCollection<User> Users { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+        public User SelectedUser { get; set; }
 
         public UserViewModel()
         {
             LoadUsers();
+            DeleteCommand = new RelayCommand(o => OnDelete(SelectedUser), o => CanDelete(SelectedUser));
         }
 
         private void LoadUsers()
@@ -23,6 +26,18 @@ namespace AppWithGui.ViewModel
             };
 
             Users = users;
+        }
+
+        private void OnDelete(object user)
+        {
+            var u = user as User;
+            Users.Remove(u);
+        }
+
+        private bool CanDelete(object user)
+        {
+            var u = user as User;
+            return u != null;
         }
     }
 }
